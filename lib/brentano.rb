6bd34504b08b9913a2fd5c410363f2a631a35c27
Roberto Thais <roberto.n.thais@gmail.com>
2011-01-24 20:55:35 -0500

    added some customization

--- lib/brentano.rb
+++ lib/brentano.rb
@@ -8,4 +8,10 @@ module Brentano
   mattr_accessor :query_limit
   @@query_limit = 20
   
+  mattr_accessor :qualifier_key
+  @@qualifier_key = :options
+  
+  mattr_accessor :presenter_module
+  @@presenter_module = "Presenters"  
+  
 end
--- lib/brentano/rails/helpers.rb
+++ lib/brentano/rails/helpers.rb
@@ -1,8 +1,8 @@
 module Brentano
   module Helpers
     
-    def presenter(klass_name, subject, options = params[:options], &blk) 
-      klass = "Presenters::#{klass_name.to_s.camelcase}".constantize
+    def presenter(klass_name, subject, options = params[Brentano.qualifier_key], &blk) 
+      klass = "#{Brentano.presenter_module}::#{klass_name.to_s.camelcase}".constantize
       klass.new subject, options, &blk
     end
   
